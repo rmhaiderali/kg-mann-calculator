e7f041b8f62925090359b09edcf68cb108ef8c4f
Haider Ali <rmha62@gmail.com>
2026-06-11 23:28:38 +0500

    Add PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,29 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/kg-mann-calculator",
   plugins: [
     react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
+      babel: { plugins: [["babel-plugin-react-compiler"]] },
+    }),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: { enabled: false },
+      manifest: {
+        name: "Calculator",
+        short_name: "Calculator",
+        theme_color: "#131313",
+        icons: [
+          {
+            src: "icon.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
       },
     }),
   ],
-  base: "/kg-mann-calculator",
 })
