b1b7ff72c372d62fb1611271e6446878789132af
Haider Ali <rmha62@gmail.com>
2026-06-12 15:16:57 +0300

    Extract query-state and refactor text state

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import { useEffect, useState, useRef } from "react"
 import { useSelect } from "downshift"
 import debounce from "debounce"
+import {
+  pushState,
+  replaceState,
+  normalizeToString,
+  getCurrentParamsObject,
+} from "./query-state.js"
 
 function uppercaseFirstChar(str) {
   return str.charAt(0).toUpperCase() + str.slice(1)
 }
 
-const newLine = "\n"
-const newLineReplace = ":"
-
-function toSearchParams(params) {
-  return new URLSearchParams(params)
-    .toString()
-    .replaceAll(encodeURIComponent(newLineReplace), newLineReplace)
-}
+const originalNewLine = "\n"
+const newLine = "+"
 
 const debounce_1_50ms = debounce((fn) => fn(), 50)
 const debounce_1_500ms = debounce((fn) => fn(), 500)
@@ -37,13 +37,6 @@ function App() {
 
   const [lineHeight, setLineHeight] = useState(calculateLineHeight)
 
-  const [searchParams, setSearchParams] = useState(() => {
-    const params = Object.fromEntries(new URLSearchParams(location.search))
-    params.text = params.text?.replaceAll(newLine, newLineReplace) || ""
-    history.replaceState(params, "", "?" + toSearchParams(params))
-    return params
-  })
-
   function updateTextRefWidth() {
     if (isFieldSizingSupported) return
     textRef.current.style.width = "0px"
@@ -57,34 +50,17 @@ function App() {
   }
 
   function updateLineNoRefContent(text) {
-    const linesCount = text.split(newLineReplace).length
-    lineNoRef.current.value = arrayRange(1, linesCount).join(newLine)
-    updateLineNoRefWidth()
+    const linesCount = text.split(newLine).length
+    lineNoRef.current.value = arrayRange(1, linesCount).join(originalNewLine)
   }
 
-  function updateBothRefWidth() {
+  function updateLineHeight() {
     setLineHeight(calculateLineHeight())
-    updateTextRefWidth()
-    updateLineNoRefWidth()
   }
 
-  const setText = (newText) => {
-    debounce_1_50ms(() => {
-      updateLineNoRefContent(newText)
-      updateLineNoRefWidth()
-    })
-    updateTextRefWidth()
-    setSearchParams((prev) => {
-      const params = { ...prev, text: newText }
-      debounce_1_500ms(() =>
-        history.pushState(params, "", "?" + toSearchParams(params)),
-      )
-      return params
-    })
-  }
+  const [text, setText] = useState("")
 
-  const text = searchParams?.text || ""
-  const lines = text.split(newLineReplace)
+  const lines = text.split(newLine)
   const numbers = lines.map((line) => parseFloat(line))
   const sum = numbers.reduce((a, b) => a + (isNaN(b) ? 0 : b), 0)
 
@@ -101,12 +77,27 @@ function App() {
   }
 
   useEffect(() => {
+    const currentParamsObject = getCurrentParamsObject()
+    const text = currentParamsObject.text || ""
+    setText(text)
+    replaceState({ text }, currentParamsObject)
     updateLineNoRefContent(text)
-    updateBothRefWidth()
-    window.addEventListener("resize", updateBothRefWidth)
+
+    updateTextRefWidth()
+    updateLineNoRefWidth()
+
+    window.addEventListener("resize", () => {
+      updateLineHeight()
+      updateTextRefWidth()
+      updateLineNoRefWidth()
+    })
+
     window.addEventListener("popstate", ({ state }) => {
-      setSearchParams(state)
-      updateLineNoRefContent(state.text)
+      const text = normalizeToString(state.text)
+      setText(text)
+
+      updateTextRefWidth()
+      updateLineNoRefWidth()
     })
   }, [])
 
@@ -168,10 +159,17 @@ function App() {
           ref={textRef}
           rows={lines.length}
           style={{ flex: "1 1 auto" }}
-          value={text.replaceAll(newLineReplace, newLine)}
-          onChange={(e) =>
-            setText(e.target.value.replaceAll(newLine, newLineReplace))
-          }
+          value={text.replaceAll(newLine, originalNewLine)}
+          onChange={(e) => {
+            const text = e.target.value.replaceAll(originalNewLine, newLine)
+            setText(text)
+            updateTextRefWidth()
+            debounce_1_50ms(() => {
+              updateLineNoRefContent(text)
+              updateLineNoRefWidth()
+            })
+            debounce_1_500ms(() => pushState({ text }))
+          }}
           inputMode={selectedItem}
         />
       </div>
---
+++ src/query-state.js
@@ -0,0 +1,61 @@
+const newLine = "+"
+const newLineEncoded = encodeURIComponent(newLine)
+
+export function getCurrentParamsObject() {
+  const currentParams = new URLSearchParams(
+    location.search.replaceAll(newLine, newLineEncoded),
+  )
+  return Object.fromEntries(currentParams)
+}
+
+export function pushState(
+  object,
+  currentParamsObject = getCurrentParamsObject(),
+) {
+  const nextParamsObject = Object.assign(currentParamsObject, object)
+
+  for (const key in nextParamsObject)
+    if (nextParamsObject[key] === "") delete nextParamsObject[key]
+
+  const nextParams = new URLSearchParams(nextParamsObject)
+
+  const nextParamsStr = nextParams
+    .toString()
+    .replaceAll(newLineEncoded, newLine)
+
+  const url = location.pathname + (nextParamsStr ? "?" + nextParamsStr : "")
+
+  history.pushState(nextParamsObject, "", url)
+}
+
+export function replaceState(
+  object,
+  currentParamsObject = getCurrentParamsObject(),
+) {
+  const nextParamsObject = Object.assign(currentParamsObject, object)
+
+  for (const key in nextParamsObject)
+    if (nextParamsObject[key] === "") delete nextParamsObject[key]
+
+  const nextParams = new URLSearchParams(nextParamsObject)
+
+  const nextParamsStr = nextParams
+    .toString()
+    .replaceAll(newLineEncoded, newLine)
+
+  const url = location.pathname + (nextParamsStr ? "?" + nextParamsStr : "")
+
+  history.replaceState(nextParamsObject, "", url)
+}
+
+export function normalizeToString(value) {
+  return value?.toString() || ""
+}
+
+// export function wrapStateSetter(fn) {
+//   return (fnOrValue) => {
+//     typeof fnOrValue === "function"
+//       ? fn((prev) => fnOrValue(prev)?.toString() || "")
+//       : fn(fnOrValue?.toString() || "")
+//   }
+// }
